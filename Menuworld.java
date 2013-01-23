c7dfcac815dc9525ec535bbfb5070981df116e3b
Levi Cupedo <0868671@hr.nl>
2013-01-23 13:35:53 +0100

    inhoud

---
+++ Menuworld.java
@@ -0,0 +1,30 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Menuworld here.
+ * 
+ * @author (Levi Cupedo) 
+ * @version (0.001)
+ */
+public class Menuworld extends World
+{
+
+    /**
+     * Constructor for objects of class Menuworld.
+     * 
+     */
+    public Menuworld()
+    {    
+        // Create a new world with 800x800 cells with a cell size of 1x1 pixels.
+        super(80, 80, 10);
+        setBackground("Menu.png");
+        
+    }
+    
+    public void menufill()
+    {
+        
+        
+        
+    }
+}
