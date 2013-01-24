f15f6e788ed74521487543b36b14f73971c1c2e8
Levi Cupedo <0868671@hr.nl>
2013-01-24 16:54:39 +0100

    Extra scoreboard buttons and other gameimages

--- Menuworld.java
+++ Menuworld.java
@@ -22,11 +22,19 @@ public class Menuworld extends World
         // Add things to menu
         addObject(new Game1(), 40, 70);
         addObject(new Game2(), 120, 70);
-        addObject(new Game3(), 40, 120);
-        addObject(new Game4(), 120, 120);
-        addObject(new Scoreboardbutton(), 20, 150);
+        addObject(new Game3(), 40, 125);
+        addObject(new Game4(), 120, 125);
+        addObject(new Scoreboardbutton1(), 40, 80);
+        addObject(new Scoreboardbutton2(), 120, 80);
+        addObject(new Scoreboardbutton3(), 40, 135);
+        addObject(new Scoreboardbutton4(), 120, 135);
         addObject(new Quitbutton(), 145, 150);
         
+        //images of games
+        addObject(new GameImage1(), 40, 50);
+        addObject(new GameImage2(), 120, 50);
+        addObject(new GameImage3(), 40, 105);
+        addObject(new GameImage4(), 120, 105);
     }
     
 }
--- Scoreboardbutton2.java
+++ Scoreboardbutton2.java
@@ -1,15 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Game1 here.
+ * Write a description of class Scoreboardbutton2 here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Scoreboardbutton extends Buttons
+public class Scoreboardbutton2 extends Buttons
 {
     /**
-     * Act - do whatever the Game1 wants to do. This method is called whenever
+     * Act - do whatever the Scoreboardbutton2 wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
